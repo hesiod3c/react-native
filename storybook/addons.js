2b27633a70b2919b5ba6035bdbf28191c6b058de
Hemerson Vianna <hemerson.lourenco@gmail.com>
2017-08-17 02:43:35 -0300

    feature(storybook): add addons

--- storybook/addons.js
+++ storybook/addons.js
@@ -1,4 +1,4 @@
-/* eslint-disable import/no-extraneous-dependencies, import/no-unresolved, import/extensions */
-
+import '@storybook/addons';
 import '@storybook/addon-actions/register';
 import '@storybook/addon-links/register';
+import '@storybook/addon-options/register';
