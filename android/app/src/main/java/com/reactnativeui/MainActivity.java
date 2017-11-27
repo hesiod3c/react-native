2d05a0ff7137686e2f77f4c181c0efff72df468a
Hemerson Vianna <hemerson.lourenco@gmail.com>
2017-11-26 23:48:01 -0200

    feat: update structure

--- android/app/src/main/java/com/reactnativeui/MainActivity.java
+++ android/app/src/main/java/com/reactnativeui/MainActivity.java
@@ -1,4 +1,4 @@
-package com.uireactnative;
+package com.reactnativeui;
 
 import com.facebook.react.ReactActivity;
 
@@ -10,6 +10,6 @@ public class MainActivity extends ReactActivity {
      */
     @Override
     protected String getMainComponentName() {
-        return "uireactnative";
+        return "reactnativeui";
     }
 }
