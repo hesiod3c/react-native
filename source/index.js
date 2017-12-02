8ea953d66161aec6c5ab2a8c9c5dadb34d15dfe7
Hemerson Vianna <hemerson.lourenco@gmail.com>
2017-12-02 13:58:27 -0200

    fix: update index

--- source/index.js
+++ source/index.js
@@ -1,8 +1,12 @@
+import Body from './body';
+import Container from './container';
 import Svg from './svg';
 import Button from './button';
 import Touchable from './touchable';
 
 module.exports = {
+  Body,
+  Container,
   Svg,
   Button,
   Touchable,
